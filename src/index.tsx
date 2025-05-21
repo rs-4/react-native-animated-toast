d599b2877d26ac534785f55cb8c0ef980e769930
rs4 <rayanselmi100@gmail.com>
2025-05-21 23:55:31 +0200

    feat: update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
-export function multiply(a: number, b: number): number {
-  return a * b;
-}
+import { useNotif, NotificationProvider } from './library/index';
+
+export * from './library/index';
+export { default as DynamicIslandNotification } from './library/DynamicIslandNotifications';
+export { default as NotchNotification } from './library/NotchNotification';
+export { default as ToastNotification } from './library/ToastNotification';
+
+export default {
+  Provider: NotificationProvider,
+  useNotification: useNotif,
+};
